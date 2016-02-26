2b197403920b34394e38ede23fbc002a47562758
David <davbohr@gmail.com>
2016-02-26 16:33:05 +0100

    a few bits of css integration

--- components/Obd1.js
+++ components/Obd1.js
@@ -3,28 +3,27 @@ require("./obd1.less");
 
 
 export default function Obd1() {
-  return <div className="container">
-          <div className="jumbotron row">
-            <div className="col-md-2"></div>
-            <div className="col-md-6">
-            <h1>Bienvenue</h1>
-            <p>Merci pour votre inscription à Edge Factory
+  return <div className="container Obd1-container">
+          <div className="jumbotron ">
+            <div className="Obd1-inner-container">
+
+            <h1 className="Obd1-title">Bienvenue</h1>
+            <p className="Obd1-description">Merci pour votre inscription à Edge Factory
             </p>
-            <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. 
+            <p className="Obd1-description">This is a template showcasing the optional theme stylesheet included in Bootstrap. 
             Use it as a starting point to create something more unique by building on or modifying it.
             </p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            <p className="Obd1-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat.
             </p>
-            <nav>
-              <button >Suivant</button>
+            <nav className="Obd1-nav">
+              <button className="Obd1-button-next">Suivant</button>
             </nav>
-            </div>
-            <div className="col-md-2"></div>
         	
 
+          </div>
       		</div>
       	</div>
 }
